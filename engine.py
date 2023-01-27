fd54fcee1b3b584c576d6c9311e95aa16579a05c
Skullheadx <admonty1@gmail.com>
2023-01-27 14:17:07 -0500

    keyboard listener

--- engine.py
+++ engine.py
@@ -1,5 +1,7 @@
 from grid import Grid
 from renderer import Renderer
+import pynput.keyboard as keyboard
+import threading
 
 
 class Engine:
@@ -12,3 +14,39 @@ class Engine:
         self.renderer.render(self.grid)
         self.grid.update()
 
+    class Event:
+        events = []
+        keys = {}
+
+        KEYDOWN = 0
+        KEYUP = 1
+        QUIT = 2
+
+        def on_press(self, key):
+            if key not in self.keys:
+                self.keys[key] = False
+            if self.keys[key]:
+                return
+            self.keys[key] = True
+
+            if key == keyboard.Key.esc:
+                self.events.append((self.QUIT, None))
+            else:
+                self.events.append((self.KEYDOWN, key))
+
+        def on_release(self, key):
+            if not self.keys[key]:
+                return
+            self.keys[key] = False
+            self.events.append((self.KEYUP, key))
+
+        def __init__(self):
+            listener = keyboard.Listener(
+                on_press=self.on_press,
+                on_release=self.on_release)
+            listener.start()
+
+        def get(self):
+            output = self.events.copy()
+            self.events.clear()
+            return output
--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     is_running = True
 
     while is_running:
+        for event, key in engine.Event().get():
+            if event == engine.Event.QUIT:
+                is_running = False
+                break
+
         engine.update()
         time.sleep(0.1)
 
