10fc3dde9ee93d23539a12d95aafa37afe236d37
Skullheadx <admonty1@gmail.com>
2023-01-27 14:28:18 -0500

    add comments + event listener destructor

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import time
 from terminalengine import TerminalEngine
 
 
-def main():
-    window = TerminalEngine()
+def main():  # main function
+    window = TerminalEngine()  # create the window
 
     is_running = True
-
-    while is_running:
-        for event, key in window.Event().get():
-            if event == window.Event.QUIT:
-                is_running = False
+    while is_running:  # main loop
+        for event, key in window.event.get():  # event loop
+            if event == window.Event.QUIT:  # quit event
+                is_running = False  # quit
                 break
 
-        window.update()
-        time.sleep(0.1)
+        window.update()  # update the window
+        time.sleep(0.1)  # sleep for 0.1 seconds
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":  # if the file is being run directly
+    main()  # run the main function
--- terminalengine.py
+++ terminalengine.py
@@ -6,47 +6,52 @@ from renderer import Renderer
 
 class TerminalEngine:
     def __init__(self):
-        self.grid = Grid()
-        self.renderer = Renderer()
-        self.renderer.render(self.grid)
+        self.grid = Grid()  # create the grid
+        self.renderer = Renderer()  # create the renderer
+        self.event = self.Event()  # create the event handler
 
-    def update(self):
+    def update(self):  # update the grid and render it
         self.renderer.render(self.grid)
         self.grid.update()
 
-    class Event:
-        events = []
-        keys = {}
+    class Event:  # event handler
+        events = []  # list of events
+        keys = {}  # dictionary of keys
 
-        KEYDOWN = 0
-        KEYUP = 1
-        QUIT = 2
+        # Key Codes
+        KEYDOWN = 0  # keydown event
+        KEYUP = 1  # keyup event
+        QUIT = 2  # quit event
 
         def on_press(self, key):
-            if key not in self.keys:
+            if key not in self.keys:  # if the key is not in the keys list, add it
                 self.keys[key] = False
-            if self.keys[key]:
-                return
-            self.keys[key] = True
 
-            if key == keyboard.Key.esc:
+            if self.keys[key]:  # if the key is already pressed, return
+                return
+            self.keys[key] = True  # set the key to pressed
+
+            if key == keyboard.Key.esc:  # if the key is the escape key, add the quit event to the events list
                 self.events.append((self.QUIT, None))
-            else:
+            else:  # otherwise, add the keydown event to the events list
                 self.events.append((self.KEYDOWN, key))
 
         def on_release(self, key):
-            if not self.keys[key]:
+            if not self.keys[key]:  # if the key is not pressed, return
                 return
-            self.keys[key] = False
-            self.events.append((self.KEYUP, key))
+            self.keys[key] = False  # set the key to not pressed
+            self.events.append((self.KEYUP, key))  # add the keyup event to the events list
 
         def __init__(self):
-            listener = keyboard.Listener(
+            self.listener = keyboard.Listener(  # create the listener
                 on_press=self.on_press,
-                on_release=self.on_release)
-            listener.start()
+                on_release=self.on_release)  # set the listener to use the on_press and on_release functions
+            self.listener.start()  # start the listener
 
         def get(self):
-            output = self.events.copy()
-            self.events.clear()
-            return output
+            output = self.events.copy()  # copy the events
+            self.events.clear()  # clear the events
+            return output  # return the events
+
+        def __del__(self):  # destructor
+            self.listener.stop()  # stop the listener
