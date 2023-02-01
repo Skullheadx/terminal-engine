31f6e59fbf72b9393a70760baa44d01826d588f7
lbcmk <30442978+lbcmk@users.noreply.github.com>
2023-02-01 14:49:39 -0500

    Updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from terminalengine import TerminalEngine
 
 def update(terminal_engine, dt):
     terminal_engine.draw.rect(10, 2, 10, 10, terminal_engine.color.RED)
+    terminal_engine.draw.ellipse(10, 15, 10, 10, terminal_engine.color.BLUE)
 
 
 def main():
